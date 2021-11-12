ed563e6a20b3eddbc6e9ad8a68e492593089e491
stelloprint <58522233+stephenlprice@users.noreply.github.com>
2021-11-12 11:38:57 -0600

    authentication and server objects added to /notifier POST endpoint

--- notifier.py
+++ notifier.py
@@ -1,4 +1,7 @@
+import os
 from flask import Flask, request
+import tableauserverclient as TSC
+from tableauserverclient.models.pagination_item import PaginationItem
 
 app = Flask(__name__)
 
@@ -9,6 +12,14 @@ def index():
 @app.route("/notifier", methods=["GET", "POST"])
 def notify():
     if request.method == "POST":
+      # creates an authorization object using environment variables
+      tableauAuth = TSC.PersonalAccessTokenAuth(os.environ["TABLEAU_PAT_NAME"], os.environ["TABLEAU_PASSWORD"], os.environ["TABLEAU_SITENAME"])
+      # server object using environment variables
+      server = TSC.Server(os.environ["TABLEAU_SERVER"])
+      # server object signs in and fetches a list of datasources
+      with server.auth.sign_in(tableauAuth):
+        allDatasources, PaginationItem = server.datasources.get()
+
       # return f"POST data: {request.values}"
       return "<h1>Working</h1>"
 
