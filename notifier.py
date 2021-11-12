14d215cfc8d1465dc3d8d522d0f26eafcf74aa06
stelloprint <58522233+stephenlprice@users.noreply.github.com>
2021-11-11 18:08:27 -0600

    testing flask server on heroku

--- notifier.py
+++ notifier.py
@@ -5,10 +5,12 @@ app = Flask(__name__)
 @app.route("/notifier", methods=["GET", "POST"])
 def notify():
     if request.method == "POST":
-      return f"POST data: {request.values}"
-    
+      # return f"POST data: {request.values}"
+      return "<h1>Working</h1>"
+
     else: # Debugging
-      return f"GET data: {request.values}"
+      # return f"GET data: {request.values}"
+      return "<h1>Working</h1>"
 
 if __name__ == "__main__":
   app.run()
