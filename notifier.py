00f0dde6d1570e14742b22c3a6a0ec453c597f43
stelloprint <58522233+stephenlprice@users.noreply.github.com>
2021-11-14 15:37:50 -0600

    twilio client can send SMS, WhatsApp and initiate phone calls - all activity is logged

--- notifier.py
+++ notifier.py
@@ -1,4 +1,4 @@
-import os
+import os, datetime
 from flask import Flask, request, redirect, url_for
 from dotenv import load_dotenv
 import tableauserverclient as TSC
@@ -21,7 +21,7 @@ env_vars = [
   "TWILIO_ACCOUNT_SID",
   "TWILIO_AUTH_TOKEN",
   "TWILIO_FROM_NUMBER",
-  "TO_NUMBER",
+  "TWILIO_TO_NUMBER",
   "WHATSAPP_FROM",
   "WHATSAPP_TO"
 ]
@@ -35,6 +35,15 @@ for vars in env_vars:
     # output the first environment variable to fail and shut the server down
     raise RuntimeError(f"Environment variable {vars} is not available, server shutting down...")
 
+twilioSID = env_dict["TWILIO_ACCOUNT_SID"]
+twilioAuthToken = env_dict["TWILIO_AUTH_TOKEN"]
+twilioClient = Client(twilioSID, twilioAuthToken)
+fromNumber = env_dict["TWILIO_FROM_NUMBER"]
+toNumber = env_dict["TWILIO_TO_NUMBER"]
+fromWhatsApp = env_dict["WHATSAPP_FROM"]
+toWhatsApp = env_dict["WHATSAPP_TO"]
+
+
 # an API index listing supported endpoints and methods
 @app.route("/")
 def index():
@@ -47,22 +56,48 @@ def notify():
         tableauAuth = TSC.PersonalAccessTokenAuth(env_dict["TABLEAU_PAT_NAME"], env_dict["TABLEAU_PASSWORD"], env_dict["TABLEAU_SITENAME"])
         # server object using environment variables
         server = TSC.Server(env_dict["TABLEAU_SERVER"])
-        # server object signs in and fetches a list of datasources
-        with server.auth.sign_in(tableauAuth):
-          allDatasources, PaginationItem = server.datasources.get()
+        # append response data to log.txt
+        with open("log.txt", "a") as logFile:
+          # using now() to get current time for timestamp
+          current_time = datetime.datetime.now()
+          # server object signs in and fetches a list of datasources
+          with server.auth.sign_in(tableauAuth):
+            allDatasources, PaginationItem = server.datasources.get()
+            # append each 
+            logFile.write(f"{current_time}: There are {PaginationItem.total_available} datasources on site\n")
         
-        # loop through datasources on site to output an individual detail
-        for dataSource in allDatasources:
-          msgStr = f"Datasource Refresh failed\n\tName:{dataSource.name}\n\tDescription:{dataSource.description}\n\tLast updated: {dataSource.updated_at}\n"
-          with open("log.txt", "a") as logFile:
-            logFile.write(msgStr)
+            # loop through datasources on site to output an individual detail
+            for dataSource in allDatasources:
+              # append each datasource to the log file
+              msgStr = f"Datasource Refresh failed\n\tName:{dataSource.name}\n\tDescription:{dataSource.description}\n\tLast updated: {dataSource.updated_at}\n"
+              logFile.write(msgStr)
 
-        # return f"POST data: {request.values}"
-        return "<h1>Working</h1>"
+              textMessage = twilioClient.messages.create(
+                body = msgStr,
+                from_ = fromNumber,
+                to = toNumber
+              )
+              logFile.write(f"Text message SID: {textMessage.sid}\nSending SMS message from {fromNumber} to {toNumber}\n")
+
+              whatsappMessage = twilioClient.messages.create(
+                body = msgStr,
+                from_ = fromWhatsApp,
+                to = toWhatsApp
+              )
+              logFile.write(f"Whatsapp message SID: {whatsappMessage.sid}\nSending Whatsapp message from {fromWhatsApp} to {toWhatsApp}\n")
+
+              call = twilioClient.calls.create(
+                twiml = f"<Response><Say>{msgStr}</Say></Response>",
+                from_ = fromNumber,
+                to = toNumber 
+              )
+              logFile.write(f"Call SID: {call.sid}\nAutomated call from {fromNumber} to {toNumber}\n")
+
+        return "200 SUCCESS"
 
 
     else:
-      # redirects to index "/" to display a list of supported API endpoints
+      # GET requests redirects to index "/" to display a list of supported API endpoints
       return redirect(url_for("index"))
 
 if __name__ == "__main__":
