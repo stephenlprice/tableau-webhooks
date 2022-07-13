b596cab8aa32c5d8463060c87808f079f5469653
stelloprint <58522233+stephenlprice@users.noreply.github.com>
2022-07-13 02:27:45 -0500

    authenticated into rest api withn jwt and obtained a list of views

--- connected-apps.py
+++ connected-apps.py
@@ -1,6 +1,7 @@
 import os, datetime, uuid
 from dotenv import load_dotenv
 import jwt
+import requests
 
 # load environment files from .env
 load_dotenv("./.env")
@@ -59,3 +60,46 @@ decodedToken = jwt.decode(
   algorithms = header_data["alg"]
 )
 print(f'decoded token: {decodedToken}')
+
+
+# authentication into tableau's REST API
+base_path = f'{env_dict["TABLEAU_SERVER"]}/api'
+
+auth_url = f'{base_path}/3.16/auth/signin'
+
+auth_payload = """
+<tsRequest>
+  <credentials jwt="{0}">
+    <site contentUrl="{1}"/>
+  </credentials>
+</tsRequest>
+"""
+
+auth_headers = {
+  'Content-Type': 'application/xml',
+  'Accept': 'application/json'
+}
+
+response = requests.request("POST", auth_url, headers=auth_headers, data=auth_payload.format(token, env_dict["TABLEAU_SITENAME"]))
+
+response_body = response.json()
+print(response_body)
+
+# obtain dict values from response
+site_id = response_body["credentials"]["site"]["id"]
+api_key = response_body["credentials"]["token"]
+print(site_id)
+
+# get a list of broadcast views for a site
+views_url = f'{base_path}/exp/sites/{site_id}/views'
+
+payload={}
+headers = {
+  'Accept': 'application/json',
+  'X-Tableau-Auth': api_key
+}
+
+response = requests.request("GET", views_url, headers=headers, data=payload)
+
+print(response.text)
+
