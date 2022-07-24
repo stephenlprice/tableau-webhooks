bb40d191893cae8d220cb6ed21afb00c95a49e03
stelloprint <58522233+stephenlprice@users.noreply.github.com>
2022-07-24 17:23:40 -0500

    added an exceptions.py file to implement custom exceptions

---
+++ utils/exceptions.py
@@ -0,0 +1,91 @@
+from utils import log
+
+class Error(Exception):
+  """base class for errors"""
+
+
+class EnvironmentAttributeError(Error):
+  """
+  Exception raised when environment variables are empty strings
+
+  Attributes:
+    key_attribute -- environment variable
+  """
+
+  def __init__(self, environment_variable):
+    self.environment_variable = environment_variable
+    self.message = f"Environment variable value for key {environment_variable} was not assigned."
+    self.log = log.logger.error(f"Environment variable value for key {environment_variable} was not assigned.")
+    super().__init__(self.message)
+
+
+class EnvironmentKeyError(Error):
+  """
+  Exception raised when the environment variables dict does not have required keys
+
+  Attributes:
+    key_attribute -- environment variable
+  """
+
+  def __init__(self, vars):
+    self.environment_variable = vars
+    self.message = f"Environment variable {vars} was not declared."
+    self.log = log.logger.error(f"Environment variable {vars} was not declared.")
+    super().__init__(self.message)
+
+
+class JWTEncodingError(Error):
+  """
+  Exception raised when encoding fails
+
+  Attributes:
+    key_attribute -- error
+  """
+
+  def __init__(self, error):
+    self.message = f"Cannot encode JWT: {error}"
+    self.log = log.logger.error(f"Cannot encode JWT, {error}")
+    super().__init__(self.message)
+
+
+class JWTDecodingError(Error):
+  """
+  Exception raised when decoding fails
+
+  Attributes:
+    key_attribute -- error
+  """
+
+  def __init__(self, error):
+    self.message = f"Cannot decode JWT: {error}"
+    self.log = log.logger.error(f"Cannot decode JWT, {error}")
+    super().__init__(self.message)
+
+
+class TableauRestAuthError(Error):
+  """
+  Exception raised when authentication to Tableau's REST API fails
+
+  Attributes:
+    key_attribute -- error
+  """
+
+  def __init__(self, error):
+    self.message = f"Authentication to Tableau REST API failed: {error}"
+    self.log = log.logger.error(f"Authentication to Tableau REST API failed, {error}")
+    super().__init__(self.message)
+
+
+class TableauRestError(Error):
+  """
+  Exception raised when requests to Tableau's REST API fail
+
+  Attributes:
+    key_attribute -- error
+  """
+
+  def __init__(self, error):
+    self.message = f"Request to Tableau REST API failed: {error}"
+    self.log = log.logger.error(f"Request to Tableau REST API failed, {error}")
+    super().__init__(self.message)
+    
