2f5548497d3311042f8d7a501adca9a96d4895ee
stelloprint <58522233+stephenlprice@users.noreply.github.com>
2022-07-24 17:22:51 -0500

    added a logs file to manage logging

---
+++ utils/log.py
@@ -0,0 +1,11 @@
+import logging
+
+# instantiate a logger object writing to connected-apps.log
+logging.basicConfig(
+  format='%(asctime)s,%(msecs)d %(levelname)-8s [%(filename)s:%(lineno)d] %(message)s',
+  datefmt='%Y-%m-%d:%H:%M:%S',
+  level=logging.DEBUG,
+  filename='logs/connected_apps.log'
+)    
+# logger object named after module: https://docs.python.org/3/howto/logging.html#advanced-logging-tutorial
+logger = logging.getLogger(__name__)
