0bfb0affaf9f5f3e9d8f3738fd38076163eff7f7
stelloprint <58522233+stephenlprice@users.noreply.github.com>
2022-07-25 02:34:02 -0500

    added methods to list broadcasts and update them

--- modules/broadcast.py
+++ modules/broadcast.py
@@ -1,7 +1,7 @@
 from utils import log
 from libs import connected_apps, tableau_rest
 
-def update(env_dict):
+def update(env_dict, workbook_id):
   # encode a JWT token for connected apps authentication: https://help.tableau.com/current/online/en-us/connected_apps.htm#step-4-embedding-next-steps
   jwt = connected_apps.encode(env_dict)
   print('SUCCESS: jwt encoded...')
@@ -12,7 +12,13 @@ def update(env_dict):
   print('SUCCESS: REST API key obtained...')
   log.logger.info('SUCCESS: REST API key obtained...')
 
-  # get a list of workbooks on the site
-  workbooks = tableau_rest.get_workbooks_site(api_key)
-  print('SUCCESS: Workbooks queried...')
-  log.logger.info('SUCCESS: Workbooks by site queried...')
+  # get all broadcasts on the site
+  broadcasts = tableau_rest.get_broadcasts(api_key)
+  print('SUCCESS: broadcasts views queried...')
+  log.logger.info('SUCCESS: broadcasts views queried...')
+
+  # update the broadcast
+  tableau_rest.update_broadcast(api_key, broadcasts, workbook_id, False, False)
+  print('SUCCESS: broadcast updated...')
+  log.logger.info('SUCCESS: broadcast updated...')
+  
