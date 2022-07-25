04aaf884757eabca49d78b9493c6736172de9156
stelloprint <58522233+stephenlprice@users.noreply.github.com>
2022-07-25 00:03:39 -0500

    moved all twilio notification code to libs

--- index.py
+++ index.py
@@ -1,11 +1,7 @@
-import os, datetime
+import os
 from dotenv import load_dotenv
 from flask import Flask, request, redirect, url_for
-import tableauserverclient as TSC
-from tableauserverclient.models.pagination_item import PaginationItem
-from twilio.rest import Client
 from modules import broadcast
-from libs import tableau_rest
 from utils import log, environment
 
 # dictionary with required environment variables
@@ -37,15 +33,6 @@ environment.validate(env_dict, env_vars)
 print('SUCCESS: environment validation passed...')
 log.logger.info('SUCCESS: environment validation passed...')
 
-# twilio variables
-twilioSID = env_dict["TWILIO_ACCOUNT_SID"]
-twilioAuthToken = env_dict["TWILIO_AUTH_TOKEN"]
-twilioClient = Client(twilioSID, twilioAuthToken)
-fromNumber = env_dict["TWILIO_FROM_NUMBER"]
-toNumber = env_dict["TWILIO_TO_NUMBER"]
-fromWhatsApp = env_dict["WHATSAPP_FROM"]
-toWhatsApp = env_dict["WHATSAPP_TO"]
-
 # initiate the Flask instance
 app = Flask(__name__)
 
@@ -59,6 +46,7 @@ def index():
 def broadcast():
   if request.method == "POST":
     print(request)
+    # this method updates workbooks published to broadcast
     broadcast.update(env_dict)
     return "200 SUCCESS"
 
@@ -70,62 +58,21 @@ def broadcast():
     # any other methods are not supported
     return "400 Bad Request: method not supported"
 
-# handles notifications for failed data source refreshes
-@app.route("/notifier", methods=["GET", "POST"])
+# handles notifications for failed workbook refreshes
+@app.route("/workbook-refresh-fail", methods=["GET", "POST"])
 def notify():
-    print(request)
-    if request.method == "POST":
-        # creates an authorization object using environment variables
-        tableauAuth = TSC.PersonalAccessTokenAuth(env_dict["TABLEAU_PAT_NAME"], env_dict["TABLEAU_PAT_SECRET"], env_dict["TABLEAU_SITENAME"])
-        # server object using environment variables
-        server = TSC.Server(env_dict["TABLEAU_SERVER"])
-        # append response data to log.txt
-        with open("log.txt", "a") as logFile:
-          # using now() to get current time for timestamp
-          current_time = datetime.datetime.now()
-          # server object signs in and fetches a list of datasources
-          with server.auth.sign_in(tableauAuth):
-            allDatasources, PaginationItem = server.datasources.get()
-            # append each 
-            logFile.write(f"\n{current_time}: There are {PaginationItem.total_available} datasources on site\n")
-        
-            # loop through datasources on site to output an individual detail
-            for dataSource in allDatasources:
-              # append each datasource to the log file
-              msgStr = f"Datasource Refresh failed\n\tName:{dataSource.name}\n\tDescription:{dataSource.description}\n\tLast updated: {dataSource.updated_at}\n"
-              logFile.write(msgStr)
+  print(request)
+  if request.method == "POST":  
+  
+    return "200 SUCCESS"
 
-              # implementing SMS, WhatsApp and Phone call Twilio services along with logs
-              textMessage = twilioClient.messages.create(
-                body = msgStr,
-                from_ = fromNumber,
-                to = toNumber
-              )
-              logFile.write(f"Text message SID: {textMessage.sid}\nSending SMS message from {fromNumber} to {toNumber}\n")
+  elif request.method == "GET":
+    # GET requests redirects to index "/" to display a list of supported API endpoints
+    return redirect(url_for("index"))
 
-              whatsappMessage = twilioClient.messages.create(
-                body = msgStr,
-                from_ = fromWhatsApp,
-                to = toWhatsApp
-              )
-              logFile.write(f"Whatsapp message SID: {whatsappMessage.sid}\nSending Whatsapp message from {fromWhatsApp} to {toWhatsApp}\n")
-
-              call = twilioClient.calls.create(
-                twiml = f"<Response><Say>{msgStr}</Say></Response>",
-                from_ = fromNumber,
-                to = toNumber 
-              )
-              logFile.write(f"Call SID: {call.sid}\nAutomated call from {fromNumber} to {toNumber}\n")
-
-        return "200 SUCCESS"
-
-    elif request.method == "GET":
-      # GET requests redirects to index "/" to display a list of supported API endpoints
-      return redirect(url_for("index"))
-
-    else:
-      # any other methods are not supported
-      return "400 Bad Request: method not supported"
+  else:
+    # any other methods are not supported
+    return "400 Bad Request: method not supported"
 
 if __name__ == "__main__":
   app.run()
