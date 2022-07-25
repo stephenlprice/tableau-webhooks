e1952afa98721e3ba2b4f95a36ffcc5ea9bca600
stelloprint <58522233+stephenlprice@users.noreply.github.com>
2022-07-25 00:09:38 -0500

    created functions for SMS, whatsapp and calls using the Twilio client

--- libs/notify.py
+++ libs/notify.py
@@ -1,18 +1,9 @@
 from twilio.rest import Client
 
-# twilio variables
-twilioSID = env_dict["TWILIO_ACCOUNT_SID"]
-twilioAuthToken = env_dict["TWILIO_AUTH_TOKEN"]
-fromNumber = env_dict["TWILIO_FROM_NUMBER"]
-toNumber = env_dict[""]
-fromWhatsApp = env_dict["WHATSAPP_FROM"]
-toWhatsApp = env_dict["WHATSAPP_TO"]
-
 # msgStr = f"Datasource Refresh failed\n\tName:{dataSource.name}\n\tDescription:{dataSource.description}\n\tLast updated: {dataSource.updated_at}\n"
 
-def sms(env_dict):
+def sms(env_dict, msgStr):
   twilioClient = Client(env_dict['TWILIO_ACCOUNT_SID'], env_dict['TWILIO_AUTH_TOKEN'])
-  msgStr = ''
   textMessage = twilioClient.messages.create(
     body = msgStr,
     from_ = env_dict['TWILIO_FROM_NUMBER'],
@@ -20,18 +11,19 @@ def sms(env_dict):
   )
   # logFile.write(f"Text message SID: {textMessage.sid}\nSending SMS message from {fromNumber} to {toNumber}\n")
 
+def whatsapp(env_dict, msgStr):
+  twilioClient = Client(env_dict['TWILIO_ACCOUNT_SID'], env_dict['TWILIO_AUTH_TOKEN'])
+  whatsappMessage = twilioClient.messages.create(
+    body = msgStr,
+    from_ = env_dict['WHATSAPP_FROM'],
+    to = env_dict['WHATSAPP_TO']
+  )
+  # logFile.write(f"Whatsapp message SID: {whatsappMessage.sid}\nSending Whatsapp message from {fromWhatsApp} to {toWhatsApp}\n")
 
-
-
-whatsappMessage = twilioClient.messages.create(
-  body = msgStr,
-  from_ = fromWhatsApp,
-  to = toWhatsApp
-)
-# logFile.write(f"Whatsapp message SID: {whatsappMessage.sid}\nSending Whatsapp message from {fromWhatsApp} to {toWhatsApp}\n")
-
-call = twilioClient.calls.create(
-  twiml = f"<Response><Say>{msgStr}</Say></Response>",
-  from_ = fromNumber,
-  to = toNumber 
-)
+def call(env_dict, msgStr):
+  twilioClient = Client(env_dict['TWILIO_ACCOUNT_SID'], env_dict['TWILIO_AUTH_TOKEN'])
+  call = twilioClient.calls.create(
+    twiml = f"<Response><Say>{msgStr}</Say></Response>",
+    from_ = env_dict['TWILIO_FROM_NUMBER'],
+    to = env_dict['TWILIO_TO_NUMBER']
+  )
