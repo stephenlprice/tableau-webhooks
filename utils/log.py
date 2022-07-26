341886e085f1098ff0d3ef071bcb610f355890ff
stelloprint <58522233+stephenlprice@users.noreply.github.com>
2022-07-26 13:37:28 -0500

    creating a logs.py so the directory is tracked by git

--- utils/log.py
+++ utils/log.py
@@ -5,7 +5,7 @@ logging.basicConfig(
   format='%(asctime)s,%(msecs)d %(levelname)-8s [%(filename)s:%(lineno)d] %(message)s',
   datefmt='%Y-%m-%d:%H:%M:%S',
   level=logging.DEBUG,
-  filename='logs/connected_apps.log'
+  filename='logs/webhooks.log'
 )    
 # logger object named after module: https://docs.python.org/3/howto/logging.html#advanced-logging-tutorial
 logger = logging.getLogger(__name__)
