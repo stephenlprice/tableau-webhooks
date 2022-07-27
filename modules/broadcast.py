59ed621bbfa149435300cad73c3260ca2ab9171b
stelloprint <58522233+stephenlprice@users.noreply.github.com>
2022-07-26 23:26:12 -0500

    replaced CA auth with PAT auth for broadcast

--- modules/broadcast.py
+++ modules/broadcast.py
@@ -1,11 +1,8 @@
 from libs import connected_apps, tableau_rest
 
 def update(env_dict, workbook_id):
-  # encode a JWT token for connected apps authentication: https://help.tableau.com/current/online/en-us/connected_apps.htm#step-4-embedding-next-steps
-  jwt = connected_apps.encode(env_dict)
-
   # authenticate to Tableau's REST API
-  api_key = tableau_rest.auth(env_dict, jwt)
+  api_key = tableau_rest.auth_pat(env_dict)
 
   # get all broadcasts on the site
   broadcasts = tableau_rest.get_broadcasts(api_key)
