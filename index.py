4a83ad59c53e848f4be1cc3723b367c49e334968
stelloprint <58522233+stephenlprice@users.noreply.github.com>
2022-07-27 01:47:58 -0500

    fixed path to .env file

--- index.py
+++ index.py
@@ -5,7 +5,7 @@ from modules import broadcast
 from utils import environment
 
 # load environment files from .env
-load_dotenv("../.env")
+load_dotenv(".env")
 # calling environ is expensive, this saves environment variables to a dictionary
 env_dict = dict(os.environ)
 
