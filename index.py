13ea1ad16ca5f30cf20b1f7aaa5c55d033df17d5
stelloprint <58522233+stephenlprice@users.noreply.github.com>
2022-09-14 13:34:59 -0500

    improved 400 error on broadcast update

--- index.py
+++ index.py
@@ -2,7 +2,7 @@ import os
 from dotenv import load_dotenv
 from flask import Flask, request, redirect, url_for
 from modules import broadcast
-from utils import environment, log
+from utils import environment, log, exceptions
 
 # load environment files from .env
 load_dotenv(".env")
@@ -18,7 +18,15 @@ app = Flask(__name__)
 # an API index listing supported endpoints and methods
 @app.route("/")
 def index():
-    return '<h1>Notifier API Index</h1>\n<ul><li>/notifier <strong>POST</strong> - receives incoming Tableau Server webhooks to create datasource failure notifications</li></ul>'
+  return (
+    """
+    <h1>Notifier API Index</h1>
+    <ul>
+      <li>/notifier <strong>POST</strong> - receives incoming Tableau Server webhooks to create datasource failure notifications</li>
+    </ul>
+    """, 
+    200
+  )
 
 # handles updating views on tableau broadcast once workbooks are refreshed on tableau cloud
 @app.route("/broadcast-update", methods=["GET", "POST"])
@@ -27,9 +35,16 @@ def update():
     print(request)
     log.logger.info(f"Broadcast updated: {request}")
     workbook_id = ''
-    # this method updates workbooks published to broadcast
-    broadcast.update(env_dict, workbook_id)
-    return "200 SUCCESS"
+
+    try:
+      # this method updates workbooks published to broadcast
+      broadcast.update(env_dict, workbook_id)
+    except Exception as error:
+      # use Flask's built-in logging for HTTP errors
+      app.logger.warning("Cannot update Broadcast: ", error)
+      return "500 INTERNAL SERVER ERROR", 500
+    else:
+      return "202 ACCEPTED", 202
 
   elif request.method == "GET":
     # GET requests redirects to index "/" to display a list of supported API endpoints
@@ -37,7 +52,7 @@ def update():
 
   else:
     # any other methods are not supported
-    return "400 Bad Request: method not supported"
+    return "400 BAD REQUEST: HTTP method not supported", 400
 
 # handles notifications for failed workbook refreshes
 @app.route("/workbook-refresh-fail", methods=["GET", "POST"])
@@ -54,6 +69,7 @@ def notify():
   else:
     # any other methods are not supported
     return "400 Bad Request: method not supported"
+
 
 if __name__ == "__main__":
   app.run()
