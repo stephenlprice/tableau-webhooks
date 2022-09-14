2beeadaeae6a99564dd6b57175ca36e6a69f4993
stelloprint <58522233+stephenlprice@users.noreply.github.com>
2022-09-14 13:53:15 -0500

    removed test code to solve for requests issue

--- index.py
+++ index.py
@@ -1,28 +1,8 @@
-import os, sys
+import os
 from dotenv import load_dotenv
 from flask import Flask, request, redirect, url_for
 from modules import broadcast
 from utils import environment, log
-
-def override_where():
-    """ overrides certifi.core.where to return actual location of cacert.pem"""
-    # change this to match the location of cacert.pem
-    return os.path.abspath("cacert.pem")
-
-# is the program compiled?
-if hasattr(sys, "frozen"):
-    import certifi.core
-
-    os.environ["REQUESTS_CA_BUNDLE"] = override_where()
-    certifi.core.where = override_where
-
-    # delay importing until after where() has been replaced
-    import requests.utils
-    import requests.adapters
-    # replace these variables in case these modules were
-    # imported before we replaced certifi.core.where
-    requests.utils.DEFAULT_CA_BUNDLE_PATH = override_where()
-    requests.adapters.DEFAULT_CA_BUNDLE_PATH = override_where()
 
 # load environment files from .env
 load_dotenv(".env")
