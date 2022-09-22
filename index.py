5173a74a8a78c18f874425d83e8f3344244b1b11
stelloprint <58522233+stephenlprice@users.noreply.github.com>
2022-09-22 01:33:41 -0500

    removed the / route and cleaned up the remaining routes to remove support for GET methods

--- index.py
+++ index.py
@@ -1,6 +1,6 @@
 import os
 from dotenv import load_dotenv
-from flask import Flask, request, redirect, url_for
+from flask import Flask, request
 from modules import broadcast
 from utils import environment, log
 
@@ -15,21 +15,8 @@ environment.validate(env_dict)
 # initiate the Flask instance
 app = Flask(__name__)
 
-# an API index listing supported endpoints and methods
-@app.route("/")
-def index():
-  return (
-    """
-    <h1>Notifier API Index</h1>
-    <ul>
-      <li>/notifier <strong>POST</strong> - receives incoming Tableau Server webhooks to create datasource failure notifications</li>
-    </ul>
-    """, 
-    200
-  )
-
-# handles updating views on tableau broadcast once workbooks are refreshed on tableau cloud
-@app.route("/broadcast-update", methods=["GET", "POST"])
+# updates broadcast views when workbooks refreshes trigger a webhook
+@app.route("/broadcast-update", methods=["POST"])
 def updateBroadcast():
   if request.method == "POST":
     log.logger.info(f"Broadcast workbook updated: {request}")
@@ -45,30 +32,21 @@ def updateBroadcast():
     else:
       return "202 ACCEPTED", 202
 
-  elif request.method == "GET":
-    # GET requests redirects to index "/" to display a list of supported API endpoints
-    return redirect(url_for("index"))
-
   else:
     # any other methods are not supported
     log.logger.error("400 BAD REQUEST: HTTP method not supported")
     return "400 BAD REQUEST: HTTP method not supported", 400
 
-# handles notifications for failed workbook refreshes
-@app.route("/workbook-refresh-fail", methods=["GET", "POST"])
+# sends notifications for failed workbook refreshes
+@app.route("/workbook-refresh-fail", methods=["POST"])
 def notify():
   if request.method == "POST":  
-  
     return "200 SUCCESS"
-
-  elif request.method == "GET":
-    # GET requests redirects to index "/" to display a list of supported API endpoints
-    return redirect(url_for("index"))
 
   else:
     # any other methods are not supported
-    return "400 Bad Request: method not supported"
-
+    log.logger.error("400 BAD REQUEST: HTTP method not supported")
+    return "400 BAD REQUEST: HTTP method not supported", 400
 
 if __name__ == "__main__":
   app.run()
