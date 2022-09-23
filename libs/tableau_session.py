80aee302cf018d505b4874d517a0482172d2fde3
stelloprint <58522233+stephenlprice@users.noreply.github.com>
2022-09-23 01:17:00 -0500

    refactored __str__ value for tableau session object

--- libs/tableau_session.py
+++ libs/tableau_session.py
@@ -7,12 +7,32 @@ class TableauEnv:
   authentication, other properties such as site id and api key 
   are set from the response payload.
   """
-  def __init__(self, tableau_domain, site_name, api_version):
+
+  # assign values to properties on initialization and other operations
+  def __init__(self, tableau_domain, site_name, api_version, session_minutes):
     self.paths.classic = f"{tableau_domain}/api/{api_version}"
     self.paths.new = f"{tableau_domain}/api/exp"
     self.site_name = site_name
+    self.session_minutes = session_minutes
+    self.session_date = None
     self.site_id = None
     self.api_key = None
+    
   
+  # string representation of the object
   def __str__(self):
-    return f"Tableau Session for the site {self.site_name} (ID: {self.site_id}), at {self.tableau_domain}"
+    str = """
+    Tableau Session for the site {0},
+      ID: {1}
+      Domain: {2}
+      Started: {3}
+      Duration: {4} minutes
+    """
+
+    return str.format(
+      self.site_name, 
+      self.site_id, 
+      self.tableau_domain, 
+      self.session_date, 
+      self.session_minutes
+    )
