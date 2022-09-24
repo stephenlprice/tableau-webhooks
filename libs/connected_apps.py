82866d5573d56ef5b8da49fdd1f6d6c15ba12791
stelloprint <58522233+stephenlprice@users.noreply.github.com>
2022-09-24 17:23:43 -0500

    renamed env_vars to env_dict for consistency

--- libs/connected_apps.py
+++ libs/connected_apps.py
@@ -4,24 +4,25 @@ from utils import exceptions, log
 
 
 # encode the JWT with declared payload, secret and headers
-def encode(env_vars):
-  # tableau connected app variables (JWT) see: https://help.tableau.com/current/online/en-us/connected_apps.htm#step-3-configure-the-jwt
+def encode(env_dict):
+  # tableau connected app variables (JWT)
+  # (https://help.tableau.com/current/online/en-us/connected_apps.htm#step-3-configure-the-jwt)
   header_data = {
-    "iss": env_vars["TABLEAU_CA_CLIENT"],
-    "kid": env_vars["TABLEAU_CA_SECRET_ID"],
+    "iss": env_dict["TABLEAU_CA_CLIENT"],
+    "kid": env_dict["TABLEAU_CA_SECRET_ID"],
     "alg": "HS256",
   }
 
   payload_data = {
-    "iss": env_vars["TABLEAU_CA_CLIENT"],
-    "sub": env_vars["TABLEAU_USERNAME"],
+    "iss": env_dict["TABLEAU_CA_CLIENT"],
+    "sub": env_dict["TABLEAU_USERNAME"],
     "aud": "tableau",
     "exp": datetime.datetime.utcnow() + datetime.timedelta(minutes=5),
     "jti": str(uuid.uuid4()),
     "scp": ["tableau:content:read", "tableau:workbooks:create"]
   }
 
-  connected_app_secret = env_vars["TABLEAU_CA_SECRET_VALUE"]
+  connected_app_secret = env_dict["TABLEAU_CA_SECRET_VALUE"]
 
   try:
     token = jwt.encode(
