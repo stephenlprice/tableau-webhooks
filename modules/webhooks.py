e6362ae90d0513eb5b49263451f0b46b437520c5
stelloprint <58522233+stephenlprice@users.noreply.github.com>
2022-09-26 00:37:08 -0500

    refactored all logs since logging package does not support f strings

--- modules/webhooks.py
+++ modules/webhooks.py
@@ -22,15 +22,15 @@ def workbook(payload, env_dict):
 
     # handle different webhook event types
     if event_type == "WorkbookRefreshSucceeded":
-      log.logger.info(f"Workbook Refresh Succeeded: {json.dumps(payload, indent=2, sort_keys=True)}")
+      log.logger.info(f"Workbook Refresh Succeeded: {0}".format(json.dumps(payload, indent=2, sort_keys=True)))
       # determine if the workbook should update a broadcast
       broadcast.update(env_dict, workbook_id)
 
     elif event_type == "WorkbookRefreshFailed":
-      log.logger.error(f"Workbook Refresh Failed: {json.dumps(payload, indent=2, sort_keys=True)}")
+      log.logger.error(f"Workbook Refresh Failed: {0}".format(json.dumps(payload, indent=2, sort_keys=True)))
 
     elif event_type == "WorkbookUpdated":
-      log.logger.info(f"Workbook Updated: {json.dumps(payload, indent=2, sort_keys=True)}")
+      log.logger.info(f"Workbook Updated: {0}".format(json.dumps(payload, indent=2, sort_keys=True)))
       # determine if the workbook should update a broadcast
       broadcast.update(env_dict, workbook_id)
 
