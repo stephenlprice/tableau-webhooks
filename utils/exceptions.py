c169c316539fe489c0023fffea0809fc268cc628
stelloprint <58522233+stephenlprice@users.noreply.github.com>
2022-09-26 00:36:28 -0500

    refactored all errors since python logging does not support f strings

--- utils/exceptions.py
+++ utils/exceptions.py
@@ -14,8 +14,8 @@ class EnvironmentAttributeError(Error):
 
   def __init__(self, environment_variable):
     self.environment_variable = environment_variable
-    self.message = f"Environment variable value for key {environment_variable} was not assigned."
-    self.log = log.logger.error(f"Environment variable value for key {environment_variable} was not assigned.")
+    self.message = "Environment variable value for key {0} was not assigned.".format(environment_variable)
+    self.log = log.logger.error("Environment variable value for key {0} was not assigned.".format(environment_variable))
     super().__init__(self.message)
 
 
@@ -29,8 +29,8 @@ class EnvironmentKeyError(Error):
 
   def __init__(self, vars):
     self.environment_variable = vars
-    self.message = f"Environment variable {vars} was not declared."
-    self.log = log.logger.error(f"Environment variable {vars} was not declared.")
+    self.message = "Environment variable {0} was not declared.".format(vars)
+    self.log = log.logger.error("Environment variable {0} was not declared.".format(vars))
     super().__init__(self.message)
 
 
@@ -43,8 +43,8 @@ class JWTEncodingError(Error):
   """
 
   def __init__(self, error):
-    self.message = f"Cannot encode JWT: {error}"
-    self.log = log.logger.error(f"Cannot encode JWT: {error}")
+    self.message = "Cannot encode JWT: {0}".format(error)
+    self.log = log.logger.error("Cannot encode JWT: {0}".format(error))
     super().__init__(self.message)
 
 
@@ -57,8 +57,8 @@ class JWTDecodingError(Error):
   """
 
   def __init__(self, error):
-    self.message = f"Cannot decode JWT: {error}"
-    self.log = log.logger.error(f"Cannot decode JWT: {error}")
+    self.message = "Cannot decode JWT: {0}".format(error)
+    self.log = log.logger.error("Cannot decode JWT: {0}".format(error))
     super().__init__(self.message)
 
 
@@ -71,8 +71,8 @@ class WebhookEventTypeError(Error):
   """
 
   def __init__(self, eventType):
-    self.message = f"Unexpected Webhook Event: {eventType}"
-    self.log = log.logger.error(f"Unexpected Webhook Event: {eventType}")
+    self.message = "Unexpected Webhook Event: {0}".format(eventType)
+    self.log = log.logger.error("Unexpected Webhook Event: {0}".format(eventType))
     super().__init__(self.message)
 
 
@@ -85,8 +85,8 @@ class TableauRestAuthError(Error):
   """
 
   def __init__(self, error):
-    self.message = f"Authentication to Tableau REST API failed: {error}"
-    self.log = log.logger.error(f"Authentication to Tableau REST API failed: {error}")
+    self.message = "Authentication to Tableau REST API failed: {0}".format(error)
+    self.log = log.logger.error("Authentication to Tableau REST API failed: {0}".format(error))
     super().__init__(self.message)
 
 
@@ -99,8 +99,8 @@ class TableauRestError(Error):
   """
 
   def __init__(self, error):
-    self.message = f"Request to Tableau REST API failed: {error}"
-    self.log = log.logger.error(f"Request to Tableau REST API failed: {error}")
+    self.message = "Request to Tableau REST API failed: {0}".format(error)
+    self.log = log.logger.error("Request to Tableau REST API failed: {0}".format(error))
     super().__init__(self.message)
 
 
@@ -113,8 +113,8 @@ class TableauRestGetBroadcast(Error):
   """
 
   def __init__(self, error):
-    self.message = f"Get Broadcast failed: {error}"
-    self.log = log.logger.error(f"Get Broadcast failed: {error}")
+    self.message = "Get Broadcast failed: {0}".format(error)
+    self.log = log.logger.error("Get Broadcast failed: {0}".format(error))
     super().__init__(self.message)
 
 
@@ -127,8 +127,8 @@ class TableauRestUpdateBroadcast(Error):
   """
 
   def __init__(self, error):
-    self.message = f"Update Broadcast failed: {error}"
-    self.log = log.logger.error(f"Update Broadcast failed: {error}")
+    self.message = "Update Broadcast failed: {0}".format(error)
+    self.log = log.logger.error("Update Broadcast failed: {0}".format(error))
     super().__init__(self.message)
 
     
